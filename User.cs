80d71eddc839cbc8d35c880c58132fe4b9c8fad0
DorenS <schaeferdorenm@gmail.com>
2021-05-27 23:04:21 -0500

    corrected typo/created User class, user constructors, and toString

---
+++ User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class User
+{
+	public User()
+	{
+        String myName;
+        Loan[] myLoans;
+
+        Public User()
+        {
+            name = "N/A";
+            myLoans = new Loan[0];
+        }
+
+        Public User(String name, Loan[] loans)
+        {
+            name = myName;
+            myLoans = new Loan[0];
+        }
+
+    }
+
+    public String toString()
+    {
+        //Console.WriteLine("username: " + myName);
+        //
+        String userString = "user: " +myName + " ";
+        for(int i = 0; i < myLoans.length(); i++)
+        {
+            userString += myLoans[i].toString;
+        }
+    }
+}
